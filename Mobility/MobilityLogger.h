8e68b437bca2c2b8ce63e185346d8ed051db48f7
Derrick Jones <derrickjones323@gmail.com>
2011-11-15 23:04:00 -0800

    primitive database logging
    just to verify that logger can persistently store data in the database.
    doesn't even store all the properties.

--- Mobility/MobilityLogger.h
+++ Mobility/MobilityLogger.h
@@ -9,12 +9,21 @@
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 #import <CoreMotion/CoreMotion.h>
+#import <CoreData/CoreData.h>
 
 @interface MobilityLogger : NSObject <CLLocationManagerDelegate> {
     CLLocationManager *locationManager;
     CMMotionManager *motionManager;
+    
+    NSManagedObjectContext *_managedObjectContext;
+    NSManagedObjectModel *_managedObjectModel;
+    NSPersistentStoreCoordinator *_coordinator;
 }
 
 - (void)startLoggingLocation;
 - (void)stopLoggingLocation;
+
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 @end
--- Mobility/MobilityLogger.m
+++ Mobility/MobilityLogger.m
@@ -8,7 +8,14 @@
 
 #import "MobilityLogger.h"
 
+
 @implementation MobilityLogger
+
+@synthesize managedObjectContext = _managedObjectContext;
+@synthesize managedObjectModel = _managedObjectModel;
+@synthesize persistentStoreCoordinator = _coordinator;
+
+
 
 #pragma mark - Memory Management
 - (id)init{
@@ -18,6 +25,7 @@
         locationManager.delegate = self;
 
         motionManager = [[CMMotionManager alloc] init];
+        
     }
     return self;
 }
@@ -54,6 +62,57 @@
 - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
     //FIXME: Log using CoreData
     NSLog(@"Got location update: %@, with Acceleromter data: %@", newLocation, [motionManager accelerometerData]);
+    
+    NSEntityDescription *entity = [[self.managedObjectModel entitiesByName] valueForKey:@"DataPoint"];
+
+    NSManagedObject *dataPoint = [[NSManagedObject alloc] initWithEntity:entity insertIntoManagedObjectContext:self.managedObjectContext];
+    if (dataPoint == nil)
+        abort();
+    [dataPoint setValue:[[NSNumber numberWithDouble:newLocation.coordinate.latitude] stringValue]forKey:@"latitude"];
+    
+    NSError *error = nil;
+    [self.managedObjectContext save:&error];
+    if (error) {
+        NSLog(@"error saving data point: %@", error);
+        abort();
+    }
+}
+
+#pragma mark - Core Data Stack
+// http://developer.apple.com/library/mac/#documentation/Cocoa/Conceptual/CoreDataUtilityTutorial/Articles/05_createStack.html
+- (NSManagedObjectModel *) managedObjectModel {    
+    if (_managedObjectModel != nil) {
+        return _managedObjectModel;
+    }
+    
+    // no need to specify url of model definition. this method searches for it automagically :)
+    // could be problematic if there's more than one model def. Should be fine.
+    _managedObjectModel = [[NSManagedObjectModel mergedModelFromBundles:nil] retain];
+    return  _managedObjectModel;
+}
+
++ (NSString *) documentsDirectory {
+    return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+}
+
+- (NSManagedObjectContext *) managedObjectContext {
+    if (_managedObjectContext !=nil) {
+        return _managedObjectContext;
+    }
+    
+    _managedObjectContext = [[NSManagedObjectContext alloc] init];
+    NSPersistentStoreCoordinator *coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:self.managedObjectModel];
+    [_managedObjectContext setPersistentStoreCoordinator:coordinator];
+    
+    NSURL *storeURL = [NSURL fileURLWithPath:[[MobilityLogger documentsDirectory] stringByAppendingPathComponent:@"mobility.sqlite"]];
+    NSError *error = nil;
+    NSPersistentStore *dataStore = [coordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error];
+    if (dataStore == nil) {
+        // FIXME: never programmatically quit in shipping code. Inform user there's an unrecoverable error
+        NSLog(@"error with persistent store: %@", [error localizedDescription]);
+        abort();
+    }
+    return _managedObjectContext;
 }
 
 @end
