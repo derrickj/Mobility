b49cfb183ee12381ec93af124b21831d7b79294b
Derrick Jones <derrickjones323@gmail.com>
2012-04-19 00:37:58 -0700

    add core-data generated Location class
    makes it less error prone to setup and store info

---
+++ Mobility/Location.h
@@ -0,0 +1,22 @@
+//
+//  Location.h
+//  Mobility
+//
+//  Created by Derrick Jones on 4/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Location : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * accuracy;
+@property (nonatomic, retain) NSNumber * latitude;
+@property (nonatomic, retain) NSNumber * longitude;
+@property (nonatomic, retain) NSString * provider;
+@property (nonatomic, retain) NSNumber * time;
+@property (nonatomic, retain) NSString * timezone;
+
+@end
---
+++ Mobility/Location.m
@@ -0,0 +1,21 @@
+//
+//  Location.m
+//  Mobility
+//
+//  Created by Derrick Jones on 4/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Location.h"
+
+
+@implementation Location
+
+@dynamic accuracy;
+@dynamic latitude;
+@dynamic longitude;
+@dynamic provider;
+@dynamic time;
+@dynamic timezone;
+
+@end
