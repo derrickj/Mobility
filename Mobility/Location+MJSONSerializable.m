1ca49be4d3734f01547113ad437c3869c348513c
Derrick Jones <derrickjones323@gmail.com>
2012-04-24 15:33:19 -0700

    implement -[Location serializableRepresentation]
    fixes test on CoreDataObjectTests

--- Mobility/Location+MJSONSerializable.m
+++ Mobility/Location+MJSONSerializable.m
@@ -9,5 +9,15 @@
 #import "Location+MJSONSerializable.h"
 
 @implementation Location (MJSONSerializable)
-- (id)serializableRepresentation { return  nil; } //FIXME: implement
+- (id)serializableRepresentation {
+    NSMutableDictionary *d = [[NSMutableDictionary alloc] init];
+    [d setValue:self.accuracy forKey:@"accuracy"];
+    [d setValue:self.latitude forKey:@"latitude"];
+    [d setValue:self.longitude forKey:@"longitude"];
+    [d setValue:self.provider forKey:@"provider"];
+    [d setValue:self.time forKey:@"time"];
+    [d setValue:self.timezone forKey:@"timezone"];
+    [d setValue:self.uuid forKey:@"id"]; // NOTE: use "id" for the key, because that's what Ohmage server expects
+    return [d autorelease];
+}
 @end
