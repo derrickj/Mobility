9052286445bea9dae184b35e28445e77017cfc70
Derrick Jones <derrickjones323@gmail.com>
2012-04-24 14:39:14 -0700

    add stub for AccelData JSON serialization

---
+++ Mobility/AccelData+MJSONSerializable.h
@@ -0,0 +1,14 @@
+//
+//  AccelData+MJSONSerializable.h
+//  Mobility
+//
+//  Created by Derrick Jones on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AccelData.h"
+#import "MJSONSerializable.h"
+
+@interface AccelData (MJSONSerializable)<MJSONSerializable>
+
+@end
---
+++ Mobility/AccelData+MJSONSerializable.m
@@ -0,0 +1,13 @@
+//
+//  AccelData+MJSONSerializable.m
+//  Mobility
+//
+//  Created by Derrick Jones on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AccelData+MJSONSerializable.h"
+
+@implementation AccelData (MJSONSerializable)
+- (id)serializableRepresentation { return  nil; } //FIXME: implement
+@end
